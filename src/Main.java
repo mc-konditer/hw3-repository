ee6f1845563c9a48b753246b01c858a31a487829
ziryan <ziryan@bk.ru>
2023-12-09 16:56:36 +0800

    hw3

--- src/Main.java
+++ src/Main.java
@@ -46,6 +46,48 @@ public class Main {
         int brownPaint = allClass * 4;
         System.out.println("В школе, где " + allClass + " классов, нужно " + whitePaint + " банок белой краски и " + brownPaint + " банок коричневой краски");
 
+        int bananas = 5;
+        int weightBanana = 80;
+        int milk100 = 2;
+        int weightMilk100 = 105;
+        int iceCream = 2;
+        int weightIceCream = 100;
+        int eggs = 4;
+        int weightEgg = 70;
+        int weightBananas = bananas * weightBanana;
+        int weightMilk200 = weightMilk100 * milk100;
+        int weightIceCream2 = weightIceCream * iceCream;
+        int weightEggs = weightEgg * eggs;
+        int weightBreakfast = weightBananas + weightMilk200 + weightIceCream2 + weightEggs;
+        int grPerKg = 1000;
+        float weightBreakfastKg = (float) weightBreakfast / grPerKg;
+        System.out.println("Вес завтрака спортсменов в граммах равен " + weightBreakfast + " гр.");
+        System.out.println("Вес завтрака спортсменов в киллограммах равен " + weightBreakfastKg + " кг.");
+
+        int excessWeight = 7;
+        int weightLoss1 = 250;
+        int weightLoss2 = 500;
+        int days1 = excessWeight * 1000 / weightLoss1;
+        int days2 = excessWeight * 1000 / weightLoss2;
+        System.out.println("При потере каждый день по 250 гр. спортсмену понадобится " + days1 + " дней.");
+        System.out.println("При потере каждый день по 500 гр. спортсмену понадобится " + days2 + " дней.");
+        int daysSr = (days1 + days2) / 2;
+        System.out.println("В среднем спортсмену понадобится " + daysSr + " день.");
+
+        int salaryMasha = 67760;
+        int salaryDenis = 83690;
+        int salaryKristina = 76230;
+        double increase = 1.1;
+        int newSalaryMasha = (int) (salaryMasha * increase);
+        int newSalaryDenis = (int) (salaryDenis * increase);
+        int newSalaryKristina = (int) (salaryKristina * increase);
+        int annualSalaryMashaBefore = salaryMasha * 12;
+        int annualSalaryMashaAfter = (int) (newSalaryMasha * 12);
+        int annualSalaryMashaDiff = annualSalaryMashaAfter - annualSalaryMashaBefore;
+        int annualSalaryDenisDiff = 12 * (newSalaryDenis - salaryDenis);
+        System.out.println("«Маша теперь получает " + newSalaryMasha + " рублей в месяц. Годовой доход вырос на " + annualSalaryMashaDiff + " рублей.");
+        System.out.println("«Денис теперь получает " + newSalaryDenis + " рублей в месяц. Годовой доход вырос на " + annualSalaryDenisDiff + " рублей.");
+        System.out.println("«Кристина теперь получает " + newSalaryKristina + " рублей в месяц. Годовой доход вырос на " + (newSalaryKristina * 12 - salaryKristina * 12) + " рублей.");
 
 
 
